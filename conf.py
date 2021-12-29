a85736d58f8ab67ceba12c629b6f26adabe9afa1
Alexandre René <alexandre.rene@freeneurons.ca>
2021-12-29 01:02:24 +0100

    Update excluded files in config

--- conf.py
+++ conf.py
@@ -6,9 +6,9 @@
 author = 'Alexandre René'
 comments_config = {'hypothesis': False, 'utterances': False}
 copyright = '2021'
-exclude_patterns = ['**.ipynb_checkpoints', '*.egg-info', '*.mypy_cache', '*.smt', '.*', '.DS_Store', 'Optimize_WF_template', 'Thumbs.db', '_build', 'data', 'labnotes', 'lib', 'reports', 'src']
+exclude_patterns = ['**.DS_Store', '**.egg-info', '**.ipynb_checkpoints', '**.smt', '**__pycache__', '**sinn.graphcache', '*.mypy_cache', '.*', '.DS_Store', 'Thumbs.db', '_build', 'data', 'envs', 'labnotes', 'lib', 'reports', 'run', 'src']
 execution_allow_errors = True
-execution_excludepatterns = ['Optimize_WF_template*']
+execution_excludepatterns = ['envs']
 execution_in_temp = False
 execution_timeout = 30
 extensions = ['sphinx_togglebutton', 'sphinx_copybutton', 'myst_nb', 'jupyter_book', 'sphinx_thebe', 'sphinx_comments', 'sphinx_external_toc', 'sphinx.ext.intersphinx', 'sphinx_panels', 'sphinx_book_theme', 'sphinx_jupyterbook_latex']
